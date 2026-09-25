17cb6ca2d98c51f5e2036502728e32df51476328
Kuldeepmobile <jainkulsabilara@gmail.com>
2026-09-25 10:10:45 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://huousyafsqmfjazwjphz.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_dGH5vs_CIFYDmL1PezBoWw_hpcpJhm5";
+const SUPABASE_BUCKET = "Skin-photo";
+const SUPABASE_TABLE = "skin_photos";
